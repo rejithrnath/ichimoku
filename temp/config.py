531784791f81c6a35fb2ced5b8c59433cbcca8b9
Rejith Reghunathan <rejithrnath@gmail.com>
2021-07-31 09:28:01 +0200

    Delete config.py

--- temp/config.py
+++
@@ -1,4 +0,0 @@
-sender_email = "rejithrnath@gmail.com"
-#receiver_email = ['220777@usn.no', 'msandmael@gmail.com']
-receiver_email = ['rejithrnath@gmail.com', '220777@usn.no']
-password = "ezzrmmyseklpypsg"
