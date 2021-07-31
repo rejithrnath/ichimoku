233309ba81b701d6e9808b3451e4e04e77ff4313
Rejith Reghunathan <220777@usn.no>
2021-07-31 09:23:02 +0200

    ichimoku code long and short

--- ichimoku_stratergy.py
+++ ichimoku_stratergy.py
@@ -245,7 +245,17 @@ def main():
     
     print("RUNNING ICHIMOKU TRADING STRATEGY !!")
     download_and_email()
-    
+    trading_time = ["09","10","11","12","13","14","15","16","17"]
+    for x in trading_time:
+        schedule.every().monday.at(str(x)+":15").do(download_and_email)
+        schedule.every().tuesday.at(str(x)+":15").do(download_and_email)
+        schedule.every().wednesday.at(str(x)+":15").do(download_and_email)
+        schedule.every().thursday.at(str(x)+":15").do(download_and_email)
+        schedule.every().friday.at(str(x)+":15").do(download_and_email)
+ 
+    while True:
+        schedule.run_pending()
+        time.sleep(1)    
     
 if __name__ == "__main__":
     main()
